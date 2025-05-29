5866007eb5de1fb82cdf776acd37dc5b380132a7
Peter Schindler <45672907+peterschindler@users.noreply.github.com>
2025-05-30 01:36:25 +0200

    Larger update to bump to Python 3.10+ and proper packaging and tests. Moved to VSCode and UV.  (#2)
    * First version before restructuring
    
    * Change error handling.
    
    * Restructured code to pip installable python package and OOP.
    
    * Updated readme and added zenodo links.
    
    * Updated package name without dashes. Fixed file imports in package.
    
    Added zenodo link to error message.
    
    * Updated package to Python 3.10+, updated package versions, and restructured to src folder structure with uv. Added MIT license. Dynamic version number. Had to update model files as well (largely the same as before in terms of performance). Moved tests from main.py to tests folder. Improved some typing.
    
    * Restored newest README.md
    
    * Updated code documentation and README.md (added bib, logo and documentation). Added ruff.toml for linting.
    
    * Made model filename an optional variable of __init__. Updated README accordingly and updated Zenodo link and most recent model filename.
    
    * Fixed sklearn warnings. Added docstrings to init and test_main.py
    
    * Increase logo size

---
+++ src/wfrfmodel/__init__.py
@@ -0,0 +1,6 @@
+"""Initialization file for the wfrfmodel package."""
+
+from .main import WFRFModel
+from ._version import __version__
+__author__ = 'Peter Schindler'
+
--- wfrfmodel/__init__.py
+++ wfrfmodel/__init__.py
@@ -1,4 +1,5 @@
 from .main import WFRFModel
-__version__ = "2.2.2"
+
+__version__ = "2.1.0"
 __author__ = 'Peter Schindler'
 
